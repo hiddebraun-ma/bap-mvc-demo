3482173c71216cfed443f7a16b07869d099fdb30
Hidde Braun <hidde.braun@gmail.com>
2020-05-18 15:25:49 +0200

    Layout en views

--- private/controllers/WebsiteController.php
+++ private/controllers/WebsiteController.php
@@ -18,5 +18,4 @@ class WebsiteController {
 
 	}
 
-
 }
---
+++ private/views/website.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="<?php echo site_url( '/css/style.css' ) ?>" media="all">
+</head>
+<body>
+<header>
+    <h1>Website titel</h1>
+</header>
+<nav>
+    Hoofdmenu
+</nav>
+<main>
+    <section class="content">
+        <h2>Pagina titel</h2>
+        Pagina inhoud
+    </section>
+    <aside>
+        <div class="top-10">
+           Sidebar
+        </div>
+    </aside>
+</main>
+
+<footer>
+    &copy; 2020
+</footer>
+</body>
+</html>
+
